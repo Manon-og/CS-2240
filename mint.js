253917a587b2aa6fd27c4216121913566bd5ab6d
James A. Manon-og <115532696+Manon-og@users.noreply.github.com>
2024-02-21 02:52:44 +0800

    Delete mint.js

--- mint.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === 'b') {
-        lenk();
-    }
-});
-
-function lenk() {
-    location.href = "https://shopee.ph/list/doublemint";
-}
